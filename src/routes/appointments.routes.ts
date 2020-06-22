ce0b4da69b7f605b5141018839fc686f718e682b
MarcosPBrandao <brandaomarcosp@gmail.com>
2020-06-22 16:05:27 -0300

    criando repositórios (gobarber)

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,24 +1,24 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
-import Appointment from '../models/appointment'; 
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
 const appointmentsRouter = Router();
 
-const appointments: Appointment[] = [];
-
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.post('/', (request, response) => {
     const { provider, date } = request.body;
     const parsedDate = startOfHour(parseISO(date));
-    const findAppointmentInStateDate = appointments.find(appointment => 
-        isEqual(parsedDate, appointment.date),
-    )
+    const findAppointmentInStateDate = appointmentsRepository.findByDate(
+        parsedDate,
+    );
     if (findAppointmentInStateDate) {
         return response
           .status(400)
           .json({ message: 'This appointment is already bookend' });
     }
-    const appointment = new Appointment (provider,parsedDate);
-    appointments.push(appointment)
+    const appointment = appointmentsRepository.create(provider, parsedDate);
    return response.json(appointment); 
 })
 
