b751cbaef7aca694a2cacbcacbb1ae5a7872a577
MarcosPBrandao <brandaomarcosp@gmail.com>
2020-06-22 13:45:03 -0300

    validando a data (gobarber)

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,16 +1,33 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+    id: string,
+    provider: string,
+    date: Date
+ }
+ 
+const appointments: Appointment[] = [];
+
 
 appointmentsRouter.post('/', (request, response) => {
     const { provider, date } = request.body;
+    const parsedDate = startOfHour(parseISO(date));
+    const findAppointmentInStateDate = appointments.find(appointment => 
+        isEqual(parsedDate, appointment.date),
+    )
+    if (findAppointmentInStateDate) {
+        return response
+          .status(400)
+          .json({ message: 'This appointment is already bookend' });
+    }
     const appointment = {
         id: uuid(),
         provider,
-        date,
+        date: parsedDate,
     }
     appointments.push(appointment)
    return response.json(appointment); 
